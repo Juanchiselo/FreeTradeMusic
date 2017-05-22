1c4c4fd7ace1693df6e75208a907e4fcc39c5a31
Joshua Itagaki <jyitagaki@cpp.edu>
2017-05-22 15:16:37 -0700

    Smart Contract credentials and information

--- src/FreeTradeMusic/Blockchain.java
+++ src/FreeTradeMusic/Blockchain.java
@@ -1,8 +1,13 @@
 package FreeTradeMusic;
 
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.WalletUtils;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.methods.response.Web3ClientVersion;
 import org.web3j.protocol.http.HttpService;
+
+import static org.web3j.tx.ManagedTransaction.GAS_PRICE;
+import static org.web3j.tx.Transfer.GAS_LIMIT;
 
 //On Command line: geth --rpcapi personal,db,eth,net,web3 --rpc --testnet
 
@@ -22,6 +27,27 @@ public class Blockchain extends Thread
             Web3j web3 = Web3j.build(new HttpService());
             Web3ClientVersion clientversion = web3.web3ClientVersion().sendAsync().get();
             System.out.println("Client is running version: " + clientversion.getWeb3ClientVersion());
+
+            Credentials credentials = WalletUtils.loadCredentials("password", "/path/to/walletfile");
+
+            //This is used to create and deploy smart contract
+            /*YourSmartContract contract = YourSmartContract.deploy(
+                    <web3j>, <credentials>,
+                    GAS_PRICE, GAS_LIMIT,
+                    <initialEtherValue>,
+                    <param1>, ..., <paramN>).get();  // constructor params*/
+
+            //This is used for existing smart contract
+            /*YourSmartContract contract = YourSmartContract.load(
+                    "0x<address>", <web3j>, <credentials>, GAS_PRICE, GAS_LIMIT);*/
+
+            //This is used to transact with smart contract
+            /*TransactionReceipt transactionReceipt = contract.someMethod(
+                    new Type(...),
+             ...).get();*/
+
+            //This is how to call a smart contract
+            /*Type result = contract.someMethod(new Type(...), ...).get();*/
         }
         catch(Exception e)
         {
