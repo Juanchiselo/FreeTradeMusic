a51d3fd57093f3d3e2c44fab99bb34ba83eb592c
Joshua Itagaki <jyitagaki@cpp.edu>
2017-06-06 17:46:55 -0700

    implemented 20% off album buy

--- src/FreeTradeMusic/Solidity/SmartContract.sol
+++ src/FreeTradeMusic/Solidity/SmartContract.sol
@@ -3,25 +3,35 @@ pragma solidity ^0.4.0;
 
 contract SmartContract {
     address artist;
-    uint value;
+    uint valueSong;
+    uint valueAlbum;
+    uint valueSongTotal;
     address buyer;
     enum State {Created, Confirmed, Disabled}
     State state;
 
     function SmartContract() payable{
         artist = msg.sender;
-        value = msg.value/2;    //price
+        valueSong = msg.value/2;    //price
+        valueSongTotal += valueSong;
+        valueAlbum = valueSongTotal - (valueSongTotal/5);
     }
 
     function buySong() payable{
         if(state != State.Created) throw;
-        if(msg.value != 2*value) throw; //value sent not enough send back to buyer
+        if(msg.value != 2*valueSong) throw; //value sent not enough send back to buyer
         buyer = msg.sender;
         state  = State.Confirmed;
     }
+    function buyAlbum() payable{
+        if(state != State.Created) throw;
+        if(msg.value != valueAlbum) throw;
+        buyer = msg.sender;
+        state = State.Confirmed;
+    }
     function confirmPurchase(){
         if(msg.sender != buyer) throw;  //the one buying the song confirms purchase
-        if(!buyer.send(value)) throw;
+        if(!buyer.send(valueSong)) throw;
         if(!artist.send(this.balance)) throw;
         state = State.Disabled;
     }
